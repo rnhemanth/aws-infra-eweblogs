08903e3f134a54b0335dc02975315dcbfd2998ae
Nagarajan <hemanth.nagarajan@optum.com>
2025-06-09 21:36:39 +0100

    initial commit

---
+++ aws-infra-deploy-eweblogs-develop/eweblogs/terraform/locals.tf
@@ -0,0 +1,4 @@
+locals {
+  ou_path               = var.environment == "prd" ? "OU=SQL_Servers,OU=IBI,OU=EMIS${upper(var.environment)}SS,DC=shared-services,DC=emis-web,DC=com" : "OU=SQL_Servers,OU=IBI,OU=EMIS${upper(var.environment)}SS,DC=${var.environment},DC=shared-services,DC=emis-web,DC=com"
+  instance_service_name = "EWEBLOGS"
+}
