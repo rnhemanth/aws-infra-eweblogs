118af5f1d564c1397620f71f01c7bd3dd644e12a
Hemanth <83559483+rnhemanth@users.noreply.github.com>
2025-06-12 11:51:25 +0100

    Delete eweblogs directory

--- eweblogs/terraform/network/locals.tf
+++
@@ -1,6 +0,0 @@
-locals {
-  transit_gateway_name = "${var.environment}-core-net-tgw-attach-${var.name.service}-${var.name.identifier}"
-  vpc_endpoint_name    = "${var.environment}-core-net-vpce-${var.name.service}-${var.name.identifier}"
-  security_group_name  = "${var.environment}-core-net-sg-vpce-${var.name.service}-${var.name.identifier}"
-  domain_credentials   = var.domain_password_secret_arn != "" ? jsondecode(data.aws_secretsmanager_secret_version.domain_creds[0].secret_string) : null
-}
