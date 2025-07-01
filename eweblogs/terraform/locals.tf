9e70ebb548a2fcda2a99a463e7573ed3e5d25478
Hemanth <83559483+rnhemanth@users.noreply.github.com>
2025-07-01 12:01:53 +0100

    Delete eweblogs directory

--- eweblogs/terraform/locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  ou_path               = var.environment == "prd" ? "OU=SQL_Servers,OU=IBI,OU=EMIS${upper(var.environment)}SS,DC=shared-services,DC=emis-web,DC=com" : "OU=SQL_Servers,OU=EWEBLOGS,OU=EMIS${upper(var.environment)}SS,DC=${var.environment},DC=shared-services,DC=emis-web,DC=com"
-  instance_service_name = "EWEBLOGS"
-}
