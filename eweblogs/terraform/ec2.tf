1d030f65dd80f63d29c07915ebbc7dd59e7b83fd
Hemanth <83559483+rnhemanth@users.noreply.github.com>
2025-07-10 10:59:44 +0100

    Add files via upload

---
+++ eweblogs/terraform/ec2.tf
@@ -0,0 +1,120 @@
+locals {
+  instance_types = { for server in var.servers : server.name => server.instance_type }
+  default_cores = {
+    for name, instance_type in local.instance_types :
+    name => data.aws_ec2_instance_type.default[instance_type].default_cores
+  }
+}
+
+data "aws_ec2_instance_type" "default" {
+  for_each      = toset(values(local.instance_types))
+  instance_type = each.value
+}
+
+module "ec2" {
+  # checkov:skip=CKV_TF_1: "Ensure Terraform module sources use a commit hash"
+  source = "git::https://github.com/emisgroup/terraform-aws-ec2.git?ref=v0.1.4-ignore_ami"
+  for_each = { for server in var.servers : server.name => server }
+
+  name                      = "${upper(var.ec2_name_prefix)}${upper(var.region_prefix)}${upper(local.instance_service_name)}${upper(each.value.name)}"
+  vpc_id                    = data.aws_vpc.vpc.id
+  ami                       = each.value.ami_id
+  associate_public_ip_address = false
+  create_iam_instance_profile = false
+  subnet_id                 = each.value.subnet_id
+  availability_zone         = each.value.az
+  instance_type             = each.value.instance_type
+  root_kms_key_id           = module.kms.key_arn
+  default_ebs_kms_key_id    = module.kms.key_arn
+  ebs_kms_key_id            = module.kms.key_arn
+  disable_api_termination   = true
+
+  cpu_core_count            = coalesce(each.value.multithreading_enabled, false) ? null : local.default_cores[each.key]
+  cpu_threads_per_core      = each.value.server_type == "db" ? coalesce(each.value.multithreading_enabled, false) ? 2 : 1 : null
+
+  private_ip                = lookup(each.value, "primary_ip", null)
+
+  secondary_private_ips = var.sql_ag ? [
+    "${each.value.secondary_ip_1}",
+    "${each.value.secondary_ip_2}",
+    "${each.value.secondary_ip_3}"
+  ] : null
+
+  vpc_security_group_ids = [
+    data.aws_security_group.standard.id,
+    data.aws_security_group.sql.id
+  ]
+
+  iam_instance_profile = aws_iam_instance_profile.ec2_profile.name
+
+  metadata_options = {
+    http_endpoint = "enabled"
+    http_tokens   = "required"
+  }
+
+  root_block_device = [
+    {
+      encrypted   = true
+      volume_type = "gp3"
+      throughput  = 125
+      volume_size = coalesce(each.value.root_volume_size, 50)
+      iops        = 3000
+    }
+  ]
+
+  ebs_block_device = [
+    for device in [
+      {
+        device_name = "/dev/sdb"
+        volume_type = "gp3"
+        volume_size = coalesce(each.value.d_volume_size, 20)
+        throughput  = coalesce(each.value.d_throughput, 125)
+        iops        = coalesce(each.value.d_iops, 3000)
+        encrypted   = true
+        tags = {
+          DriveName   = "DATA"
+          DriveLetter = "D"
+        }
+      },
+      each.value.server_function == "eweblogs-ssis" ? {
+        device_name = "/dev/sdc"
+        volume_type = "gp3"
+        volume_size = coalesce(each.value.l_volume_size, 20)
+        throughput  = coalesce(each.value.l_throughput, 125)
+        iops        = coalesce(each.value.l_iops, 3000)
+        encrypted   = true
+        tags = {
+          DriveName   = "LOGS"
+          DriveLetter = "L"
+        }
+      } : null,
+      each.value.server_function == "eweblogs-ssis" ? {
+        device_name = "/dev/sdf"
+        volume_type = "gp3"
+        volume_size = coalesce(each.value.t_volume_size, 20)
+        throughput  = coalesce(each.value.t_throughput, 125)
+        iops        = coalesce(each.value.t_iops, 3000)
+        encrypted   = true
+        tags = {
+          DriveName   = "TEMP"
+          DriveLetter = "T"
+        }
+      } : null,
+    ] : device if device != null
+  ]
+
+  tags = {
+    server_type         = each.value.server_type
+    server_function     = each.value.server_function
+    wsus                = each.value.wsus_group
+    "${each.value.wsus_qsid}" = "${each.value.wsus_policy_group}"
+    lifecycle           = coalesce(each.value.lifecycle_tag, "production")
+    rg_service          = coalesce(each.value.rg, "aws-eweblogs")
+  }
+
+  user_data_base64 = base64encode(templatefile("${path.module}/files/user_data.ps1", {
+   DomainPassword = data.aws_secretsmanager_secret.defaultadsecret.arn
+    hostname       = "${upper(var.ec2_name_prefix)}${upper(var.region_prefix)}${upper(local.instance_service_name)}${upper(each.value.name)}"
+    OUPath         = local.ou_path
+   }))
+}
---
+++ eweblogs/terraform/network/variables.tf
@@ -0,0 +1,227 @@
+variable "domain_password_secret_arn" {
+  description = "ARN for the domain password secret in AWS secrets manager"
+  type = string
+  default = ""
+}
+  
+
+
+variable "ipv4_primary_cidr_block" {
+  description = "(Optional) The IPv4 CIDR block for the VPC."
+  type        = string
+}
+
+variable "ipv4_secondary_cidr_blocks" {
+  description = "List of secondary CIDR blocks to associate with the VPC to extend the IP Address pool"
+  type        = list(string)
+  default     = []
+}
+
+variable "kms_service" {
+  description = "short service name for kms key naming"
+  type = string
+  default = "eweb"
+}
+
+variable "public_subnets" {
+  type = map(object({
+    cidr_block           = string
+    availability_zone_id = string
+  }))
+  default = {}
+}
+
+variable "private_subnets" {
+  type = map(object({
+    cidr_block           = string
+    availability_zone_id = string
+  }))
+  default = {}
+}
+
+variable "intra_subnets" {
+  type = map(map(object({
+    cidr_block           = string
+    availability_zone_id = string
+  })))
+  default = null
+}
+
+variable "tgw_id_backbone" {
+  type        = string
+  description = "TGW Backbone ID"
+}
+
+/* variable "route53_resolver_rules" {
+  type = map(object({
+    rule_id = string
+  }))
+  description = "Route53 resolver rules to attach to VPC"
+} */
+
+/* variable "vpc_peering_connections" {
+  type = map(object({
+    peer_vpc_id   = string
+    peer_owner_id = string
+  }))
+  default = {}
+} */
+
+variable "transit_gateway_attachments" {
+  description = "Map of objects that define the transit gateway attachments to be created"
+  type = map(object({
+    vpc_id             = string
+    subnet_ids         = string
+    transit_gateway_id = string
+    #  Whether Appliance Mode support is enabled. If enabled, a traffic flow between a source and destination uses the same Availability Zone for the VPC attachment for the lifetime of that flow. Valid values: `disable`, `enable`. Default value: `disable`."
+    appliance_mode_support = string
+  }))
+  default = {}
+}
+
+variable "name" {
+  type = object({
+    environment = string
+    service     = string
+    identifier  = string
+  })
+  description = "environment, service and identifier. part of naming standards"
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "auto_accept" {
+  type        = bool
+  description = "Accept the peering (both VPCs need to be in the same AWS account and region)"
+  default     = false
+}
+
+variable "wsus_rg_prefix" {
+  type        = string
+  description = "resource group prefix i.e eweblogs"
+  default     = "eweb_ibi"
+}
+
+variable "domain_credentials" {
+  description = "Domain Credentials in key-value format. password secret value from Managed AD Account"
+  default     = ""
+}
+
+variable "key_users" {
+  description = "A list of IAM ARNs for [key users](https://docs.aws.amazon.com/kms/latest/developerguide/key-policy-default.html#key-policy-default-allow-users)"
+  type        = list(string)
+  default     = []
+}
+
+variable "key_service_users" {
+  description = "A list of IAM ARNs for [key service users]"
+  type        = list(string)
+  default     = []
+}
+
+variable "key_administrators" {
+  description = "A list of IAM ARNs for [key administrators]"
+  type        = list(string)
+  default     = []
+}
+
+variable "environment" {
+  type        = string
+  description = "environment name i.e. dev/stg/prod"
+}
+
+
+variable "standard_sg_rules_cidr_blocks" {
+  type = map(object({
+    desc     = string
+    type     = string
+    from     = number
+    to       = number
+    protocol = string
+    cidr     = list(string)
+  }))
+  description = "Security group rules to CIDR blocks - standard."
+  default     = {}
+}
+
+variable "standard_sg_rules_self" {
+  type = map(object({
+    desc     = string
+    type     = string
+    from     = number
+    to       = number
+    protocol = string
+    self     = bool
+  }))
+  description = "Security group rules to self reference - standard."
+  default     = {}
+}
+
+variable "standard_sg_rules_security_group" {
+  type = map(object({
+    desc      = string
+    type      = string
+    from      = number
+    to        = number
+    protocol  = string
+    source_sg = string
+  }))
+  description = "Security group rules to another sg - standard."
+  default     = {}
+}
+
+variable "bastion_sg_rules_cidr_blocks" {
+  type = map(object({
+    desc     = string
+    type     = string
+    from     = number
+    to       = number
+    protocol = string
+    cidr     = list(string)
+  }))
+  description = "Security group rules to CIDR blocks - bastion."
+  default     = {}
+}
+
+variable "bastion_sg_rules_self" {
+  type = map(object({
+    desc     = string
+    type     = string
+    from     = number
+    to       = number
+    protocol = string
+    self     = bool
+  }))
+  description = "Security group rules to self reference - bastion."
+  default     = {}
+}
+
+variable "sql_sg_rules_cidr_blocks" {
+  type = map(object({
+    desc     = string
+    type     = string
+    from     = number
+    to       = number
+    protocol = string
+    cidr     = list(string)
+  }))
+  description = "Security group rules to CIDR blocks - eweblogs sql"
+  default     = {}
+}
+
+variable "sql_sg_rules_self" {
+  type = map(object({
+    desc     = string
+    type     = string
+    from     = number
+    to       = number
+    protocol = string
+    self     = bool
+  }))
+  description = "Security group rules to self reference - eweblogs sql."
+  default     = {}
+}
