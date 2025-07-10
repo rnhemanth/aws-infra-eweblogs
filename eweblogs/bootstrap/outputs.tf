899368d779938d9831f0efd538d4347cd2447bd1
Hemanth <83559483+rnhemanth@users.noreply.github.com>
2025-07-10 10:58:36 +0100

    Delete eweblogs directory

--- eweblogs/bootstrap/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "deployer_role_arn" {
-  value = aws_iam_role.github_deployer.arn
-}
--- eweblogs/terraform/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  ou_path = var.environment == "prd" ? (
-    "OU=SQL_Servers,OU=EWL,OU=MGMT,OU=EMIS${upper(var.environment)}SS,DC=shared-services,DC=emis-web,DC=com"
-  ) : (
-    "OU=SQL_Servers,OU=EWL,OU=MGMT,OU=EMIS${upper(var.environment)}SS,DC=${var.environment},DC=shared-services,DC=emis-web,DC=com"
-  )
-  instance_service_name = "EWEBLOGS"
-}
