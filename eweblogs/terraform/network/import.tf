95fc4ec5db4f3a09a482f79098946b050d04311a
Hemanth <83559483+rnhemanth@users.noreply.github.com>
2025-09-05 16:38:35 +0100

    Delete eweblogs directory

--- eweblogs/terraform/network/import.tf
+++
@@ -1,9 +0,0 @@
-import {
-  to = aws_secretsmanager_secret.defaultad
-  id = "arn:aws:secretsmanager:eu-west-2:296062593024:secret:dev-eweb-sec-sm-default-ad-znlLiv"
-}
-
-#import {
-#  to = module.kms.aws_kms_key.this[0]
-#  id = "76720a88-7b89-4dbb-a890-10da997b9069"
-#}
